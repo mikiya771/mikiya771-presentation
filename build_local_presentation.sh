648b34788660897cb70c67a0b9fb264f270f4d1c
mikiya <norep@noreply>
2023-01-03 19:49:32 +0900

    add: docker-tips html

--- build_local_presentation.sh
+++ build_local_presentation.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 set -eoux
 npx @marp-team/marp-cli@latest lambda-archs/lambda_local.md -o lambda-archs/lambda_local.html --theme marp-themes/ord.css --html
+npx @marp-team/marp-cli@latest docker/docker-tips/docker-tips.md -o docker/docker-tips/docker-tips.html --html
